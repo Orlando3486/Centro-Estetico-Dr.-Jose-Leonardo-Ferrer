cd63b3b65dca71f87ef6ee0488b5753ab5319e23
orlandozarraga <orlandozarraga31@hotmail.com>
2026-03-18 12:15:55 -0300

    Modificacion de tamaño de imagen en carrusel, se incluyen botones para cambio de imagen, disminucion de tamaño de logo, disminucion de tamaño de letra de botones del inicio, ajuste de grosor en link de navbar

--- front/vite-project/src/components/Carousel.jsx
+++ front/vite-project/src/components/Carousel.jsx
@@ -1,10 +1,92 @@
+// import { useState, useEffect } from "react";
+// import styles from "../styles/Carousel.module.css";
+// import { Link } from "react-router-dom";
+
+// <Link to="/login" className={styles.link}>
+//   Acceder
+// </Link>;
+
+// const images = [
+//   {
+//     src: "/estetica2.jpg",
+//     text: "Bienvenido a nuestra estética",
+//     link: "/aboutus",
+//   },
+//   {
+//     src: "/estetica4.jpg",
+//     text: "Descubrí nuestros servicios",
+//     link: "/register",
+//   },
+//   { src: "/estetica5.jpg", text: "Reservá tu turno", link: "/login" },
+// ];
+
+// const Carousel = () => {
+//   const [index, setIndex] = useState(0);
+//   const [fade, setFade] = useState(true);
+//   const [current, setCurrent] = useState(0);
+
+//   const nextImage = () => {
+//     setCurrent((prev) => (prev + 1) % images.length);
+//   };
+
+//   const prevImage = () => {
+//     setCurrent((prev) => (prev - 1 + images.length) % images.length);
+//   };
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setFade(false);
+
+//       setTimeout(() => {
+//         setIndex((prev) => (prev + 1) % images.length);
+//         setFade(true);
+//       }, 1000);
+//     }, 3000);
+
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <div className={styles.carousel}>
+//       {/* <a href="https://wa.me/549XXXXXXXX" className={styles.botonWhatsapp}>
+//         Reservar turno
+//       </a>; */}
+
+//       {/* Flecha izquierda */}
+//       <button className={styles.leftArrow} onClick={prevImage}>
+//         ❮
+//       </button>
+
+//       <Link to={images[index].link}>
+//         {/* Flecha izquierda */}
+//         <button className={styles.leftArrow} onClick={prevImage}>
+//           ❮
+//         </button>
+//         <img
+//           src={images[index].src}
+//           alt={`slide-${index}`}
+//           className={`${styles.imagen} ${
+//             fade ? styles.fadeIn : styles.fadeOut
+//           }`}
+//         />
+//         <button className={styles.rightArrow} onClick={nextImage}>
+//           ❯
+//         </button>
+//         <p className={styles.text}>{images[index].text}</p>
+//       </Link>
+
+//       <button className={styles.rightArrow} onClick={nextImage}>
+//         ❯
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Carousel;
+
 import { useState, useEffect } from "react";
 import styles from "../styles/Carousel.module.css";
 import { Link } from "react-router-dom";
-
-<Link to="/login" className={styles.link}>
-  Acceder
-</Link>;
 
 const images = [
   {
@@ -17,32 +99,46 @@ const images = [
     text: "Descubrí nuestros servicios",
     link: "/register",
   },
-  { src: "/estetica5.jpg", text: "Reservá tu turno", link: "/login" },
+  {
+    src: "/estetica5.jpg",
+    text: "Reservá tu turno",
+    link: "/login",
+  },
 ];
 
 const Carousel = () => {
   const [index, setIndex] = useState(0);
   const [fade, setFade] = useState(true);
 
+  const nextImage = () => {
+    setIndex((prev) => (prev + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setIndex((prev) => (prev - 1 + images.length) % images.length);
+  };
+
   useEffect(() => {
     const interval = setInterval(() => {
       setFade(false);
 
       setTimeout(() => {
-        setIndex((prev) => (prev + 1) % images.length);
+        nextImage();
         setFade(true);
-      }, 1000);
-    }, 3000);
+      }, 500);
+    }, 4000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div className={styles.carousel}>
-      {/* <a href="https://wa.me/549XXXXXXXX" className={styles.botonWhatsapp}>
-        Reservar turno
-      </a>; */}
+      {/* Flecha izquierda */}
+      <button className={styles.leftArrow} onClick={prevImage}>
+        ❮
+      </button>
 
+      {/* Imagen clickeable */}
       <Link to={images[index].link}>
         <img
           src={images[index].src}
@@ -53,6 +149,11 @@ const Carousel = () => {
         />
         <p className={styles.text}>{images[index].text}</p>
       </Link>
+
+      {/* Flecha derecha */}
+      <button className={styles.rightArrow} onClick={nextImage}>
+        ❯
+      </button>
     </div>
   );
 };
